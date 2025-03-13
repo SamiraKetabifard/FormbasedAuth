28aaa644823da0d4a15eccc102b964bd85da6a23
SamiraKetabifard <samiraketabi.fard@gmail.com>
2025-03-13 22:15:35 +0330

    first commit

---
+++ src/main/java/com/example/formbasedauthentication/FormBasedAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.formbasedauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormBasedAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormBasedAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/formbasedauthentication/FormBasedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formbasedauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormBasedAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
